0db7577678c9236cfb54b72b08c133c19cad35dd
Roey Feingold <roey.feingold@gmail.com>
2021-12-30 12:44:45 +0200

    Update edges.c-NEED TO TEST COMPARE FUNCTION

--- edges.c
+++ edges.c
@@ -4,10 +4,21 @@
 #define DIFF_W 1
 #define SAME 2
 
-int compare_edge(edge *e1, edge *e2) {
-    if (e1->src == e2->src && e1->w == e2->w &&){
-//        TO COMPLETE!!!
-    }
+enum comp {
+//    same_pos = 1, diff_pos = 2, same_w = 4, w1 = 16, w2 = 32
+    same_pos = 1, diff_pos = 0, same_w = 1, w1 = 0, w2 = 0
+};
+
+enum comp compare_pos(edge *e1, edge *e2) {
+    if (e1->src == e2->src && e1->dest == e2->dest) { return same_pos; }
+    else { return diff_pos; }
+}
+
+enum comp compare_w(edge *e1, edge *e2) {
+    if (e1->w == e2->w) { return same_w; }
+    else if (e1->w > e2->w) { return w1; }
+//           e1->w < e2->w
+    else { return w2; }
 }
 
 void init_edge_arr(edge_arr *arr, int init_size) {
@@ -30,7 +41,8 @@ void add_edge(edge_arr *arr, int src, float w, int dest) {
 
 void remove_edge(edge_arr *arr, edge e) {
     for (int i = 0; i < arr->used; ++i) {
-        if (compare_edge(&arr->e_arr[i], &e) == SAME) {
+//        if (compare_edge(&arr->e_arr[i], &e) == SAME) {
+        if (compare_w(&arr->e_arr[i], &e) & compare_w(&arr->e_arr[i], &e)) {
             edge *temp_e = (edge *) malloc(sizeof(edge));
             if (temp_e == NULL) { printf("error\n"); }
             temp_e->src = arr->e_arr[arr->used].src;
